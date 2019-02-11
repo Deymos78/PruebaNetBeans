67bf051812e15dc4b0074b206535209d194429a7
Deymos78 <andres_007_2012@hotmail.com>
2019-02-11 13:22:23 +0100

    Prueba Netbeans

--- src/prueba/juegode/la/viada/PruebaJuegodeLaViada.java
+++ src/prueba/juegode/la/viada/PruebaJuegodeLaViada.java
@@ -19,144 +19,42 @@ public class PruebaJuegodeLaViada {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        //Inicializamos un Scanner
+        //Instanciasmos un objeto de la clase Scanner
         Scanner teclado = new Scanner(System.in);
+
+        //Mostramos un mensaje inicial con instrucciones
+        gestorMovimientos.mensajeInicial();
         
-        //Constante varibale de las celulas, ya que el numero de estos variara 
-//        int cell = 5;
-//        boolean moverse = true;
-//        boolean[] estadoCell={true,true,true,true,true};1
-        System.out.println("INTRODUCE LAS DIMENSCIONES DEL TABLERO");
+        //PEDIMOS LA DIMENSION DEL TABLERO Y EL NUMERO DE CELULAS QUE TENDRA
+        //NO CONTROLAMOS LOS DATOS QUE INTRODUZCA EL USUARIO
         System.out.println("Introduce las filas");
         int tableroX=teclado.nextInt();
         System.out.println("Introduce las columnas");
         int tableroY=teclado.nextInt();
-        //Tablero y Random
-        String[][] tablero = new String [tableroX][tableroY];
-        Random aleatorio = new Random ();
+        System.out.println("Introduce el numero de celulas aleatorias");
+        int celulas=teclado.nextInt();
         
-        //CREAMOS UN TABLERO SECUNDARIO
+        //Creamos un Array Bidimensional de tipo String
+        //La dimension del Array Bidimensional dependera de los valores introducidos por el Usuario
+        String[][] tablero = new String [tableroX][tableroY];
+        
+        //CREAMOS UN TABLERO AUXILIAR CON LA MISMA DIMENSION QUE EL PRINCIPAL
         String tableroAux[][] = new String[tableroX][tableroY];
         
-        //Rellenar el tablero de asteriscos(*)
-        for (int i = 0; i < tableroX; i++) {
-            for (int j = 0; j < tableroY; j++) {
-                tablero[i][j] = "*";
-            }
-        }
+        //Rellenamos el tablero principal
+        tablero=gestorMovimientos.rellenarTablero(tableroX, tableroY, tablero);
         
-        //Declaramos una String para guardar las posiciones de las celulas
-//        int[] posicionCellX = new int[10];
-//        int[] posicionCellY = new int[10];
-//        int desplazamientoX;
-//        int desplazamientoY;
-//        int nuevaPosCellX;
-//        int nuevaPosCellY;
-         
-        //Colocamos las celulas aleatoriamente en el tablero
-        int numX;
-        int numY;
-        //Colocamos celulas aleatorias en el tablero
-        for (int i = 0; i < 10; i++) {
-            do {                
-                numX=aleatorio.nextInt(10);
-                numY=aleatorio.nextInt(10);
-            } while (!tablero[numX][numY].equals("*"));
-            tablero[numX][numY]="C";
-            //Guardamos las posiciones de las celulas
-//            posicionCellX[i] = numX;
-//            posicionCellY[i] =numY;
-        }
-        //Igualamos el tablero principal con el segundo tablero
-        for (int i = 0; i < tableroX; i++) {
-            for (int j = 0; j < tableroY; j++) {
-                //De esta forma tendremos dos tableros iguales
-                tableroAux[i][j] =tablero[i][j];
-            }
-        }
+        //Rellenamos el tablero principal de celulas aleatorias
+        tablero= gestorMovimientos.colocarCelulas(celulas, tableroX, tableroY, tablero);
         
-        //Imprimimos el tablero con todas las celulas
-        for (int i = 0; i < tableroX; i++) {
-            for (int j = 0; j < tableroY; j++) {
-                System.out.print(tablero[i][j]+" ");
-            }
-            System.out.println("");  
-        }
-        int contadorTurnos=0;
-        int contador=0;
-        do {            
-            //RECORREMOS TODO EL TABLERO
-            for (int i = 0; i < tableroX; i++) {
-                for (int j = 0; j < tableroY; j++) {
-                    //COMPROBAMOS SI ES UNA CELULA MUERTA O VIVA
-                    if(tablero[i][j].equals("C")){
-                        //RECORREMOS LOS ALREDEDORES DEL TABLERO
-                        for (int k = -1; k < 2; k++) {
-                            for (int l = -1; l < 2; l++) {
-                                //COMPROBAMOS LOS LIMITES DEL TABLERO
-                                if((i+k)<0 ||(i+k)>=tableroX || (j+l)<0 || (j+l)>=tableroY || (k==0&&l==0) ){
-                                    
-                                }else{
-                                    if(tablero[(i+k)][(j+l)].equals("C")){
-                                        contador++;
-                                    }
-                                    
-                                }
-                            }
-                        }
-                        //FIN DEL BUCLE DE ALREDEDOR
-                        
-                        //CAMBIAMOS EL VALOR DEPENDIENDO DEL CONTADOR
-                        if(contador<2){//SI TIENE MENOS DE DOS CELULAS VECINAS ESTA MUERE POR SOLEDAD
-                            tableroAux[i][j]="*";
-                            
-                        }else{
-                            if(contador>3){//SI TIENE MAS DE TRES CELULAS VECINAS ESTA MUERE POR SOBREPOBLACION
-                                tableroAux[i][j]="*";
-                            }
-                        }
-                        //RESETEAMOS EL CONTADOR
-                        contador=0;
-                    }else{
-                        for (int k = -1; k < 2; k++) {
-                            for (int l = -1; l < 2; l++) {
-                                //COMPROBAMOS LOS LIMITES DEL TABLERO
-                                if((i+k)<0 ||(i+k)>=tableroX || (j+l)<0 || (j+l)>=tableroY || (k==0&&l==0) ){
-                                    
-                                }else{
-                                    if(tablero[(i+k)][(j+l)].equals("C")){
-                                        contador++;
-                                    }
-                                }
-                            }
-                        }
-                        if(contador==3){
-                            tableroAux[i][j]="C";
-                        }
-                        //RESETEAMOS EL CONTADOPR
-                        contador=0;
-                    }
-                }
-            }
-            System.out.println("------------------------------------------------\n\n");
-            
-            //Imprimimos el tablero con todas las celulas
-            for (int i = 0; i < tableroX; i++) {
-                for (int j = 0; j < tableroY; j++) {
-                    System.out.print(tableroAux[i][j]+" ");
-                }
-                System.out.println("");  
-            }
-            
-            //AHORA COLOCAMOS EL TABLEROAUX A EL TABLERO PRINCIPAL
-            for (int i = 0; i < tableroX; i++) {
-                for (int j = 0; j < tableroY; j++) {
-                    tablero[i][j] = tableroAux[i][j];//DE ESTA FORMA LOS TABLEROS SERAN IGUALES
-                }
-            }
-            contadorTurnos++;
-        } while (contadorTurnos<3);
+        //Clonamos el tablero principal, y lo guardamos el el tablero auxiliar
+        tableroAux=gestorMovimientos.rellenarTableroAuxoPrincipal(tableroX, tableroY, tableroAux, tablero);
+        
+        //Imprimimos el tablero principal con todos sus elementos
+        gestorMovimientos.imprimirtablero(tableroX, tableroY, tablero);
+        
+        //COMENZAMOS EL JUEGO DE LA VIDA
+        gestorMovimientos.comenzarJuego(tableroX, tableroY, tablero, tableroAux);
     }
     
 }
--- src/prueba/juegode/la/viada/gestorMovimientos.java
+++ src/prueba/juegode/la/viada/gestorMovimientos.java
@@ -6,28 +6,154 @@
 package prueba.juegode.la.viada;
 
 import java.util.HashMap;
+import java.util.Random;
 
 /**
  *
  * @author andre
  */
 public class gestorMovimientos {
-    private int[] posX;
     
-    private int[] posY;
-    private int[][] posiciones;
-    HashMap<String, Integer> local = new HashMap<String, Integer>();
-
-    public gestorMovimientos(int sizeX, int sizeY) {
-        this.posiciones = new int[sizeX][sizeY];
-        this.posX = new int[10];
-        this.posY = new int[10];
+    public static void mensajeInicial(){
+        System.out.println("INTRODUCE LAS DIMENSCIONES DEL TABLERO Y EL NUMERO DE CELULAS");
     }
     
-    public void meterLocalizacion(int i, int j){
+    public static String[][] rellenarTablero(int x, int y, String tablero[][]){
+        //Rellenar el tablero de asteriscos(*)
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                tablero[i][j] = "*";
+            }
+        }
+        //Devolvemos el tablero Relleno de asteriscos
+        return tablero;
+    }
+    
+    public static String[][] colocarCelulas(int celulas,int x, int y, String tablero[][]){
+        Random aleatorio = new Random ();
+        //Colocamos las celulas aleatoriamente en el tablero
+        int numX;
+        int numY;
+        //Colocamos celulas aleatorias en el tablero
+        for (int i = 0; i < celulas; i++) {
+            do {                
+                numX=aleatorio.nextInt(x);
+                numY=aleatorio.nextInt(y);
+            } while (!tablero[numX][numY].equals("*"));
+            tablero[numX][numY]="C";
+        }
+        return tablero;
+    }
+
+    public static String[][] rellenarTableroAuxoPrincipal(int x, int y, String tableroAux[][], String tablero[][]){
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                //De esta forma tendremos dos tableros iguales
+                tableroAux[i][j] =tablero[i][j];
+            }
+        }
+        return tableroAux;
+    }
+    
+    public static void imprimirtablero(int x, int y, String[][] tablero){
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                System.out.print(tablero[i][j]+" ");
+            }
+            System.out.println("");  
+        }
+    }
+    
+    public static int contarCelulasVecinas(int x, int y, String tablero[][], int i, int j){
+        int contadorCelulasVecinas=0;
+        for (int k = -1; k < 2; k++) {
+            for (int l = -1; l < 2; l++) {
+                //COMPROBAMOS LOS LIMITES DEL TABLERO
+                if((i+k)<0 ||(i+k)>=x || (j+l)<0 || (j+l)>=y || (k==0&&l==0) ){
+
+                }else{
+                    if(tablero[(i+k)][(j+l)].equals("C")){
+                        contadorCelulasVecinas++;
+                    }
+
+                }
+            }
+        }
+        //FIN DEL BUCLE DE ALREDEDOR
+        return contadorCelulasVecinas;
+    }
+    
+    public static String[][] updatetableroAux(int estado, int i, int j, String tableroAux[][], int celulasVecinas){
+        //Dependiendo del estado la celula estara muerta o viva
+        // estado==0 ---> MUERTA
+        // estado==1 ---> VIVA
+        if(estado==1){
+            if(celulasVecinas<2){//SI TIENE MENOS DE DOS CELULAS VECINAS ESTA MUERE POR SOLEDAD
+            tableroAux[i][j]="*";
+
+            }else{
+                if(celulasVecinas>3){//SI TIENE MAS DE TRES CELULAS VECINAS ESTA MUERE POR SOBREPOBLACION
+                    tableroAux[i][j]="*";
+                }
+            }
+        }else if(estado==0){
+            if(celulasVecinas==3){
+                tableroAux[i][j]="C";
+            }
+        }
         
+        return tableroAux;
     }
+    
+    public static void comenzarJuego(int tableroX, int tableroY, String tablero[][], String tableroAux[][]){
+        //Contador para los turnos, es decir, el numero de veces que se ejecutar el bucle
+        //PUEDE SER INFINITO, PERO NO LO HACEMOS EN ESTE CASO
+        int contadorTurnos=0;
+        
+        //Contador para contar el nuemro de Celulas Vecinas
+        int contadorCelulasVecinas=0;
+        
+        do {            
+            //RECORREMOS POSICION POR POSICION EL TABLERO PRINCIPAL
+            for (int i = 0; i < tableroX; i++) {
+                for (int j = 0; j < tableroY; j++) {
+                    //COMPROBAMOS SI ES UNA CELULA VIVA O MUERTA
+                    if(tablero[i][j].equals("C")){
+                        //RECORREMOS LOS ALREDEDORES DE LA CELULA EN LA QUE NOS ENCONTREMOS
+                        contadorCelulasVecinas=contarCelulasVecinas(tableroX, tableroY, tablero, i, j);
+                        
+                        //Modificamos la celula en funcion del contador de celulas vecinas, enviamos un 1 si la celula esta viva
+                        // y un 0 si la celula esta muerta
+                        tableroAux=updatetableroAux(1, i, j, tableroAux, contadorCelulasVecinas);
+                        
+                        //RESETEAMOS EL CONTADOR
+                        contadorCelulasVecinas=0;
+                    }else{
+                        //Recorremos el alrededor de la celula en la que nos encontramos
+                        contadorCelulasVecinas=contarCelulasVecinas(tableroX, tableroY, tablero, i, j);
+                        
+                        //Modificamos la celula en funcion del contador de celulas vecinas
+                        //Enviamos un 0, ya que la celula esta muerta
+                        tableroAux=updatetableroAux(0, i, j, tableroAux, contadorCelulasVecinas);
+                        
+                        //RESETEAMOS EL CONTADOPR
+                        contadorCelulasVecinas=0;
+                    }
+                }
+            }
+            System.out.println("------------------------------------------------\n\n");
+            
+            //En ESTE CASO LE PASAMOS EL TABLERO AUXILIAR, YA QUE QUEREMOS QUE NOS IMPRIMA ESTE
+            imprimirtablero(tableroX, tableroY, tableroAux);
+            
+            //Empleamos el metod anterior, pero en este caso queremos copiar tablero principal y pegarlo en el principal
+            tablero=rellenarTableroAuxoPrincipal(tableroX, tableroY, tablero, tableroAux);
+            
+            //Esta variable nos permitira controlar el numero de repeticiones del bucle
+            contadorTurnos++;
+        } while (contadorTurnos<3);
 
+    }
     
     
     
